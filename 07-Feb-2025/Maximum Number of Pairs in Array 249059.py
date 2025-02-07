687820a8eb3cae5848ae89d4f2edf4ad82e207cf
betelcode <167538440+benayascode@users.noreply.github.com>
2025-02-07 10:17:54 +0300

    Created 'Maximum Number of Pairs in Array' by A2SV Hub

---
+++ 07-Feb-2025/Maximum Number of Pairs in Array 249059.py
@@ -0,0 +1,12 @@
+# Problem: Maximum Number of Pairs in Array - https://leetcode.com/problems/maximum-number-of-pairs-in-array/description/
+
+class Solution:
+    def numberOfPairs(self, nums: List[int]) -> List[int]:
+        mydict = Counter(nums)
+        a , b = 0 , 0
+        for i in mydict:
+            if mydict[i] % 2:
+                b += 1
+            a += (mydict[i] // 2)
+        return [a,b]
+            
