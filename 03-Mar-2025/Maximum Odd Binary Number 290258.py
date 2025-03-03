0d4613b4cd2404f98457990cab02572830a1328e
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-03-03 23:45:38 +0300

    Created 'Maximum Odd Binary Number' by A2SV Hub

---
+++ 03-Mar-2025/Maximum Odd Binary Number 290258.py
@@ -0,0 +1,9 @@
+# Problem: Maximum Odd Binary Number - https://leetcode.com/problems/maximum-odd-binary-number/
+
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        one = s.count("1")
+        return "1"*(one-1) + "0"*(len(s)-one) + "1"
+        
+
+        
