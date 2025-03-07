642c80029296d5ec4f25dd6b31fd5cb245606c6c
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-03-07 11:18:17 +0300

    Created 'Crawler Log Folder' by A2SV Hub

---
+++ 07-Mar-2025/Crawler Log Folder 295662.py
@@ -0,0 +1,11 @@
+# Problem: Crawler Log Folder - https://leetcode.com/problems/crawler-log-folder/
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for i in logs:
+            if stack and i == "../":
+                stack.pop()
+            elif i != "./" and i != "../":
+                stack.append(i)
+        return len(stack)
