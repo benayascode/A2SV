cafaff5a4819563c5bf2df2c05075558d8f87aea
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-03-09 15:17:49 +0300

    Created 'Split a String in Balanced Strings' by A2SV Hub

---
+++ 09-Mar-2025/Split a String in Balanced Strings 298565.py
@@ -0,0 +1,17 @@
+# Problem: Split a String in Balanced Strings - https://leetcode.com/problems/split-a-string-in-balanced-strings/
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        cnt = 0
+        r , l = 0 ,0 
+        for i in s:
+            if i == "L":
+                l += 1
+            else:
+
+                r += 1
+            if r == l:
+                cnt += 1
+                l = 0
+                r = 0
+        return cnt
