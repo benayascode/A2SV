249b72360386013f027c197ef27ea2b4123f4f7f
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-09-19 22:55:15 +0300

    Created 'All Ancestors of a Node in a Directed Acyclic Graph ' by A2SV Hub

---
+++ 19-Sep-2025/All Ancestors of a Node in a Directed Acyclic Graph  394285.py
@@ -0,0 +1,28 @@
+# Problem: All Ancestors of a Node in a Directed Acyclic Graph  - https://leetcode.com/problems/all-ancestors-of-a-node-in-a-directed-acyclic-graph/description/
+
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        out = [set() for _ in range(n)]
+        indeg = [0] * n
+        graph = defaultdict(list)
+
+
+        for u, v in edges:
+            graph[u].append(v)
+            indeg[v] += 1
+
+        q = deque()
+        for i in range(len(indeg)):
+            if indeg[i] == 0:
+                q.append(i)
+        while q:
+            node = q.popleft()
+            for nei in graph[node]:
+                out[nei] = out[nei].union(out[node], {node})
+                indeg[nei] -= 1
+                if not indeg[nei]:
+                    q.append(nei)
+                    
+            out[node] = sorted(out[node])
+        return out
+
