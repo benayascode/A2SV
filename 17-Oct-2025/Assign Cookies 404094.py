fbd6dd15bf0416434ddfe7c9468bdde58e99d9d0
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-10-17 20:10:28 +0300

    Created 'Assign Cookies' by A2SV Hub

---
+++ 17-Oct-2025/Assign Cookies 404094.py
@@ -0,0 +1,15 @@
+# Problem: Assign Cookies - https://leetcode.com/problems/assign-cookies
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i=0
+        j=0
+        out=0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                out+=1
+                i+=1
+            j+=1
+        return out
