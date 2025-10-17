5f2bf12c72024cd32b7a023d6d0330793467dac0
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-10-17 16:26:19 +0300

    Created 'Implement Trie (Prefix Tree)' by A2SV Hub

---
+++ 17-Oct-2025/Implement Trie (Prefix Tree) 403993.py
@@ -0,0 +1,50 @@
+# Problem: Implement Trie (Prefix Tree) - https://leetcode.com/problems/implement-trie-prefix-tree/
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.flag = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for i in word:
+            if i not in curr.children:
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+        curr.flag = True        
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for i in word:
+            if i not in curr.children:
+                return False
+            curr = curr.children[i]
+        return curr.flag
+        
+    
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for i in prefix:
+            if i not in curr.children:
+                return False
+            curr = curr.children[i]
+        return True
+
+        
+
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
