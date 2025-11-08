094253f435f6a5bbb9f862d38d4a6f8d0666efe9
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-11-08 21:40:14 +0300

    Created 'Domino Piling' by A2SV Hub

---
+++ 08-Nov-2025/Domino Piling 408023.py
@@ -0,0 +1,31 @@
+# Problem: Domino Piling - https://codeforces.com/problemset/problem/50/A
+
+import math, sys,heapq
+from bisect import bisect_left, bisect_right
+from collections import deque, defaultdict, Counter
+from random import randint
+
+ni = lambda: int(sys.stdin.readline().strip())
+nsi = lambda: list(map(int, sys.stdin.readline().strip().split()))
+wsi = lambda: sys.stdin.readline().strip().split()
+wi = lambda: sys.stdin.readline().strip()
+yn = lambda condition: 'YES' if condition else 'NO'
+test = lambda inp=0: ni() if not inp else inp
+randkey = randint(0, 100000)
+xor = lambda x: x ^ randkey
+
+'''
+
+
+
+'''
+def solve():
+    n,m = nsi()
+    print(n*m//2)
+
+
+    return
+
+ttt = 1
+for _ in range(test(ttt)):
+    solve()
