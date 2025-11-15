cbd197c11aaa5e243cd48879056fe056d987c403
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-11-15 03:35:41 +0300

    Created 'Python Lists' by A2SV Hub

---
+++ 15-Nov-2025/Python Lists 408922.py
@@ -0,0 +1,21 @@
+# Problem: Python Lists - https://www.hackerrank.com/challenges/python-lists/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    N = int(input())
+    out = []
+    for i in range(N):
+        command = input().split()
+        if command[0] == "insert":
+            out.insert(int(command[1]), int(command[2]))
+        elif command[0] == "append":
+            out.append(int(command[1]))
+        elif command[0] == "remove":
+            out.remove(int(command[1]))
+        elif command[0] == "sort":
+            out.sort()
+        elif command[0] == "print":
+            print(out)
+        elif command[0] == "pop":
+            out.pop()
+        else:
+            out.reverse()
