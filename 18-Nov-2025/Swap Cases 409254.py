ee749afb26e909c656f25aa8e90fe88e62adf584
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-11-18 21:32:33 +0300

    Created 'Swap Cases' by A2SV Hub

---
+++ 18-Nov-2025/Swap Cases 409254.py
@@ -0,0 +1,10 @@
+# Problem: Swap Cases - https://www.hackerrank.com/challenges/swap-case?isFullScreen=true
+
+def swap_case(s):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].islower():
+            s[i] = s[i].upper()
+        else:
+            s[i] = s[i].lower()
+    return "".join(s)
