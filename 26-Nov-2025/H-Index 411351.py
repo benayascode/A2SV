4e2b5540d5a6b1a771a95566ac5286f1732d1252
Betel Mekasha <167538440+benayascode@users.noreply.github.com>
2025-11-26 17:33:36 +0300

    Created 'H-Index' by A2SV Hub

---
+++ 26-Nov-2025/H-Index 411351.py
@@ -0,0 +1,19 @@
+# Problem: H-Index - https://leetcode.com/problems/h-index/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+
+        l,r = 0, n-1
+        while l <= r:
+            mid = (l+r)//2
+            if citations[mid] == n - mid:
+                return n - mid
+            elif citations[mid] < n-mid:
+                l = mid + 1
+            
+            else:
+                r = mid - 1
+        return n - l
+        
